41a70af2a337678756bae2f541e3106dc384980b
id2k1149 <id2k1149@gmail.com>
2023-05-08 21:12:37 -0400

    Initial Commit

---
+++ DotsOnCircle/DotsOnCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotsOnCircleApp.swift
+//  DotsOnCircle
+//
+//  Created by Max Franz Immelmann on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DotsOnCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
